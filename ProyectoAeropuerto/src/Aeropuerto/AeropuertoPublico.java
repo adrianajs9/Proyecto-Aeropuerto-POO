b77c40ddbbfd56c116d093e0229a516760471bc2
Adriana Jimenez <adrianitajs09@hotmail.com>
2021-10-04 16:50:54 -0500

    Create AeropuertoPublico.java
    Aeropuerto publico es un hijo de aeropuerto

---
+++ ProyectoAeropuerto/src/Aeropuerto/AeropuertoPublico.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aeropuerto;
+
+/**
+ *
+ * @author adria
+ */
+public class AeropuertoPublico extends Aeropuerto{
+    private double subvencion;
+
+    public AeropuertoPublico(String nombre, String ciudad, String pais) {
+        super(nombre, ciudad, pais);
+    }
+
+    public AeropuertoPublico(double subvencion, String nombre, String ciudad, String pais, Compania[] c) {
+        super(nombre, ciudad, pais, c);
+        this.subvencion = subvencion;
+    }
+
+    public AeropuertoPublico(double subvencion, String nombre, String ciudad, String pais) {
+        super(nombre, ciudad, pais);
+        this.subvencion = subvencion;
+    }
+
+    public double getSubvencion() {
+        return subvencion;
+    }
+    
+    
+    
+}
